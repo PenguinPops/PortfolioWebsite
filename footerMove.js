4d177f1e6998d3fed5361b0fda8b57e4d5aa212b
PenguinPops <msoltys.biz@gmail.com>
2023-07-29 12:22:27 +0200

    updated footer

--- footerMove.js
+++ footerMove.js
@@ -1,20 +1,17 @@
-const texts = document.querySelectorAll('.center-p');
-const title = document.getElementById('center-h2');
 const img = document.getElementById('footerImg');
+const block = document.getElementById('center-text');
+const title = document.getElementById('title-p');
+const insidetxt = document.getElementById('center-p');
 window.addEventListener('scroll', function() {
-
-    var scrollY = window.pageYOffset;
   
     // Update text position based on scroll
-    var move = -1 * (53 - (scrollY * 0.008) % window.innerHeight);
+    var move = -1 * (53 - (window.scrollY * 0.008) % window.innerHeight);
 
     // console.log(move);
-  
-    for (var i = 1; i <= texts.length; i++) {
-        var text = texts[i-1];
-        text.style.marginRight = 30+((move)*(i*0.5)) + 'vh';
-      }
-
-    title.style.marginRight = 40+move+'vh';
-    img.style.marginLeft = -16 + 2*move+'em';
+    block.style.marginRight = 10 + move + 'vh';
+    insidetxt.style.marginLeft = 5 - move + 'vh';
+    title.style.marginRight = 30 + 3*move + 'vh';
+    img.style.marginLeft = -16 + 2*move + 'vh';
+    title.style.opacity = 0.2+0.5*move;
+    insidetxt.style.opacity = 0.5*move;
 });
