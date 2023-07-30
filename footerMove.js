67f713f27b0b7ff4b97cbbcdfc66bcc4ca08b42a
PenguinPops <msoltys.biz@gmail.com>
2023-07-30 15:11:05 +0200

    scaling fixes

--- footerMove.js
+++ footerMove.js
@@ -7,9 +7,9 @@ window.addEventListener('scroll', function() {
     // Update text position based on scroll
     var move = -1 * (53 - (window.scrollY * 0.008) % window.innerHeight);
 
-    // console.log(move);
-    block.style.marginRight = 10 + move + 'vh';
-    insidetxt.style.marginLeft = 5 - move + 'vh';
-    title.style.marginRight = 30 + 3*move + 'vh';
-    img.style.marginLeft = -16 + 2*move + 'vh';
+    // // console.log(move);
+    // // block.style.marginRight = 10 + move + 'em';
+    // insidetxt.style.marginLeft = 5 - move + 'vw';
+    // title.style.marginRight = 30 + 3*move + 'vw';
+    // img.style.marginLeft = -16 + 2*move + 'vw';
 });
